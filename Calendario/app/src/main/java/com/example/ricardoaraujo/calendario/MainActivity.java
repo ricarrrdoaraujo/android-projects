3ab3fdbed481e45c824f336f880fa4c436631973
Ricardo <ricarrrdoluigi@gmail.com>
2019-01-31 17:18:57 -0200

    Calendário

--- Calendario/app/src/main/java/com/example/ricardoaraujo/calendario/MainActivity.java
+++ Calendario/app/src/main/java/com/example/ricardoaraujo/calendario/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CalendarView;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+
 public class MainActivity extends AppCompatActivity {
 
-    private CalendarView calendarView;
+    private MaterialCalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +22,35 @@ public class MainActivity extends AppCompatActivity {
 
         calendarView = findViewById(R.id.calendarView);
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        /*
+        //limitar data
+        calendarView.state().edit()
+                .setMinimumDate(CalendarDay.from(2015,1,1))
+                .setMaximumDate(CalendarDay.from(2018,3,1))
+                .commit();
+         */
+
+        //configurar array com nomes dos meses
+        CharSequence meses[] = {"Janeiro", "Fereveiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        //configurar semana
+        CharSequence semana[] = {"Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+        calendarView.setWeekDayLabels(semana);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                Log.i("data", "valor: " + dayOfMonth + "/" + month + "/" + year);
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Log.i("data", "valor: " + (date.getMonth() + 1) + "/" + date.getYear());
             }
         });
+
+        /*calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Log.i("data", "valor: " + date);
+            }
+        });*/
 
     }
 }
--- Organizze/app/src/main/java/com/example/ricar/organizze/activity/PrincipalActivity.java
+++ Organizze/app/src/main/java/com/example/ricar/organizze/activity/PrincipalActivity.java
@@ -7,10 +7,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.ricar.organizze.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private MaterialCalendarView calendarView;
+    private TextView textoSaudacao, textoSaldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,11 @@ public class PrincipalActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        textoSaldo = findViewById(R.id.textSaldo);
+        textoSaudacao = findViewById(R.id.textSaudacao);
+        calendarView = findViewById(R.id.calendarView);
     }
+
 
     public void adicionarDespesa(View view){
         startActivity(new Intent(this, DespesasActivity.class));
@@ -35,6 +46,17 @@ public class PrincipalActivity extends AppCompatActivity {
 
     public void adicionarReceita(View view){
         startActivity(new Intent(this, ReceitasActivity.class));
+    }
+
+    public void configurarCalendarView(){
+        CharSequence[] meses = {"Janeiro","Fevereiro","Março","Abril","Maio", "Junho","Julho","Agosto", "Setembro","Outubro","Novembro","Dezembro"};
+        calendarView.setTitleMonths(meses);
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 
 }
