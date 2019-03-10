683ac82cab33517df222f917f9ef0ecbc041a405
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2019-03-10 00:13:41 -0300

    Threads and AsyncTask

---
+++ AsyncTask/app/src/main/java/com/example/ricar/asynctask/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.ricar.asynctask;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    public void iniciarAsyncTask(View view){
+
+        MyAsyncTask task = new MyAsyncTask();
+        task.execute(10);
+
+    }
+    /* Generics
+    * 1. Parametro a ser passado para a classe
+    * 2. Tipo de valor que será utilizado para o progresso da tarefa
+    * 3. Retorno após a tarefa finalizada
+    * */
+
+    class MyAsyncTask extends AsyncTask<Integer, Integer, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+
+            int numero = integers[0];
+            for(int i=0; i < numero; i++){
+                //envia para o progress
+                publishProgress(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            return "Finalizado";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+
+    }
+}
