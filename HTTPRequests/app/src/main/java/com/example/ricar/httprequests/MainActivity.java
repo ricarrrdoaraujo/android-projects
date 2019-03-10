c76f007fa860c87d7261d520996d82a18241c708
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2019-03-10 18:22:24 -0300

    JSONObject

--- HTTPRequests/app/src/main/java/com/example/ricar/httprequests/MainActivity.java
+++ HTTPRequests/app/src/main/java/com/example/ricar/httprequests/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -35,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 String urlApi = "https://blockchain.info/ticker";
                 String cep = "13077009";
                 String urlCep = "https://viacep.com.br/ws/" + cep + "/json/ ";
-                task.execute(urlCep);
+                task.execute(urlApi);
             }
         });
     }
@@ -84,7 +87,41 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            resultado.setText(s);
+
+            /*String logradouro = null;
+            String cep = null;
+            String complemento = null;
+            String bairro = null;
+            String localidade = null;
+            String uf = null;*/
+
+            String objetoValor = null;
+            String valorMoeda = null;
+            String simbolo = null;
+
+            try {
+                /*JSONObject jsonObject = new JSONObject(s);*/
+                /*logradouro = jsonObject.getString("logradouro");
+                cep = jsonObject.getString("cep");
+                complemento = jsonObject.getString("complemento");
+                bairro = jsonObject.getString("bairro");
+                localidade = jsonObject.getString("localidade");
+                uf = jsonObject.getString("uf");*/
+
+                JSONObject jsonObject = new JSONObject(s);
+                objetoValor = jsonObject.getString("BRL");
+
+                JSONObject jsonObject1Real = new JSONObject(objetoValor);
+                valorMoeda = jsonObject1Real.getString("last");
+                simbolo = jsonObject1Real.getString("symbol");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            /*resultado.setText(logradouro + " / " + cep+ " / " + complemento+
+                    " / " + bairro + " / " + localidade + " / " + uf);*/
+            resultado.setText(simbolo + " " + valorMoeda);
         }
     }
 }
